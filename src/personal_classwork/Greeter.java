25617a18d02b301bdbdd6094f8cd6008d4ac2984
benir-o <beniromenda@gmail.com>
2025-05-05 14:05:20 +0300

    Greet the user

---
+++ src/personal_classwork/Greeter.java
@@ -0,0 +1,12 @@
+package personal_classwork;
+
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+
+    public static void main(String[] args) {
+        new Greeter().greet();
+        // Greets the user
+    }
+}
